9982f272d6265dbfbe124ae675b10cdb6d060786
geniusbat-Tytan <43751753+geniusbat@users.noreply.github.com>
2022-03-09 18:25:17 +0100

    Setup

---
+++ Block.cs
@@ -0,0 +1,22 @@
+﻿using ProofOfCredit.Transactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProofOfCredit
+{
+    class Block
+    {
+        public long stamp;
+        public List<Transaction> transactions;
+        public byte[] prevHash;
+        public Members.Miner miner;
+        public uint timeGen;
+        public byte pv;
+        public Block()
+        {
+        }
+    }
+}
